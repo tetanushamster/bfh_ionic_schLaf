2131b16907de014449bb4d2dd0590e49ab39bbb0
elly2178 <dumitru.georgiana_geanina@yahoo.com>
2019-12-04 16:12:51 +0100

    what i have done with my life

---
+++ src/app/window/que1/que1.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-que1',
+  templateUrl: './que1.page.html',
+  styleUrls: ['./que1.page.scss'],
+})
+export class Que1Page implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/window/window.page.ts
+++ src/app/window/window.page.ts
@@ -1,14 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-window',
   templateUrl: './window.page.html',
-  styleUrls: ['./window.page.scss'],
+  styleUrls: ['./window.page.scss']
 })
 export class WindowPage implements OnInit {
-  constructor() { }
+  constructor( ) { }
 
   ngOnInit() {
-  }
-
-}
+  }}
