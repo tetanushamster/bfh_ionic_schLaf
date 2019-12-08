35f2ccd8ba22f9ba3075b8ab7a7acd4b0f005286
tetanushamster <armon.dressler@gmail.com>
2019-12-09 00:17:59 +0100

    fixed merge markers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,8 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
-<<<<<<< HEAD
 import { HttpClientModule } from '@angular/common/http';
 import { OAuthModule } from 'angular-oauth2-oidc';
-=======
 /** Import needed after the firebase connection */
 import {AngularFireModule } from '@angular/fire';
 /** in enviroment is the "code" for fire */
@@ -22,7 +20,6 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
 
 
 
->>>>>>> origin/master
 
 @NgModule({
   declarations: [
@@ -32,22 +29,19 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
     BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule,
-<<<<<<< HEAD
     HttpClientModule,
     OAuthModule.forRoot({
       resourceServer: {
               allowedUrls: ['https://sleepexpert.voxte.ch/withings/identity'],
               sendAccessToken: true
               }
-            })],
-=======
+            }),
     /** add AngularFireModule.initilaizeApp to the NGModule */
     AngularFireModule.initializeApp(environment.firebase, 'bhf_ionic_schlaf'),
     AngularFireAuthModule,
     /** AngularFirestoreModule: so you can use the firestore database */
     AngularFirestoreModule
   ],
->>>>>>> origin/master
   providers: [
     StatusBar,
     SplashScreen,
