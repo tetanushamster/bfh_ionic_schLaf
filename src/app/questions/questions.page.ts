3944a7e6e33a6ead278efe9cc5b43a56e35d2a91
damichu <michael.daeppen@gmail.com>
2020-01-06 21:30:15 +0100

    new questions layout

--- src/app/questions/questions.page.ts
+++ src/app/questions/questions.page.ts
@@ -3,6 +3,7 @@ import { AngularFirestore } from '@angular/fire/firestore';
 import { FormsModule } from '@angular/forms';
 import { stringify } from 'querystring';
 import { Router } from '@angular/router';
+import { format } from 'url';
 
 @Component({
   selector: 'app-questions',
@@ -16,73 +17,73 @@ export class QuestionsPage implements OnInit {
 
   ngOnInit() {
   }
-/*
-  x=0
 
-  public btnForward(x) {
-    this.x++
-  }
-  public btnBack(x) {
-    this.x--
-  }
-  */
 data: string;
   logForm(form) {  
     return new Promise<any>((resolve, reject) =>{
     this.firestore
           .collection("protocol")
-          .add({q1:  form.a1, q2: form.a2, q3: form.a3, q4: form.a4, q5: form.a5, q6: form.a6, q7: form.a7, q8: form.a8, q9: form.a9, q10: form.a10, q11: form.a11, q12: form.a12, q13: form.a13, q14a: form.a14a, q14b: form.a14b, q14c: form.a14c, q14d: form.a14d, user: 'dummy', time: this.now})
+          .add({q1: form.a1, q2: form.a2, q3: form.a3, q4: form.a4, q5: form.a5, q6: form.a6, q7: form.a7, q8: form.a8, q9: form.a9, q10: form.a10, q11: form.a11, q12: form.a12, q13: form.a13, q14a: form.a14a, q14b: form.a14b, q14c: form.a14c, q14d: form.a14d, user: 'dummy', time: this.now})
           .then(res => {this.router.navigateByUrl('/protocol')}, err => reject(err));
         });
         
   }
 
-  /*qlist=[ { quest: 'Guten Morgen! Ich werde Ihnen nun einige Fragen stellen, um einen neuen Tagebucheintrag zu erfassen.',
-            answertype: '0',
-            nextbtn: false},
-          { quest: 'Wie war die Schlafqualität? </br> (1=sehr gut / 8=sehr schlecht)',
-            answertype: '1',
-            nextbtn: false},
-          { quest: 'Wie ist Ihr Gefühl des Erholtseins? </br> (1=sehr gut / 8=sehr schlecht)',
-            answertype: '1',
-            nextbtn: false},
-            { quest: 'Wie war Ihre Müdigkeit beim Zubettgehen? </br> (1=keine Müdigkeit / 8=starke Müdikgeit)',
-            answertype: '1',
-            nextbtn: false},
-            { quest: 'Wann sind Sie zu Bett gegangen?',
-            answertype: '2',
-            nextbtn: false},
-            { quest: 'Was haben Sie noch im Bett gemacht?',
-            answertype: '4',
-            nextbtn: false},
-            { quest: 'Wann haben Sie das Licht gelöscht?',
-            answertype: '2',
-            nextbtn: false},
-            { quest: 'Geschätzte Einschlafdauer',
-            answertype: '3',
-            nextbtn: false},
-            { quest: 'Wie oft sind Sie aufgewacht?',
-            answertype: '5',
-            nextbtn: false},
-            { quest: 'Wie lange waren Sie dann jeweils wach?',
-            answertype: '3',
-            nextbtn: false},
-            { quest: 'Wann sind Sie endgültig aufgewacht?',
-            answertype: '2',
-            nextbtn: false},
-            { quest: 'Wann sind Sie morgens aufgestanden?',
-            answertype: '2',
-            nextbtn: false},
-            { quest: 'Wie lange haben Sie geschlafen?',
-            answertype: '3',
-            nextbtn: false},
-            { quest: 'Wie lange haben Sie insgesamt im Bett gelegen?',
-            answertype: '3',
-            nextbtn: false},
-            { quest: 'Haben Sie Schlafmittel genommen?',
-            answertype: '6',
-            nextbtn: true},
-          
-          ]
-          */
+  private selectedLink: string="false";        
+  
+  setradio(e: string): void   
+  {  
+      this.selectedLink = e;        
+  }  
+  
+    isSelected(name: string): boolean   
+  {  
+          if (!this.selectedLink) {
+            return false;  
+  }  
+          return (this.selectedLink === name);
+    }  
+  
+
+  questions: Array<{ row: string, answer: string, set: boolean, nextbtn: boolean }> = [
+    { row: 'Guten Morgen! Ich werde Ihnen nun einige Fragen stellen, um einen neuen Tagebucheintrag zu erfassen.', answer: '0', set: true, nextbtn: true },
+    { row: 'Wie war die Schlafqualität? (1=sehr gut / 8=sehr schlecht)', answer: '0', set: false, nextbtn: false },
+    { row: 'Antwort 1', answer: '1', set: false, nextbtn: false },
+    { row: 'Wie ist Ihr Gefühl des Erholtseins? (1=sehr gut / 8=sehr schlecht)', answer: '0', set: false, nextbtn: false },
+    { row: 'Antwort 2', answer: '2', set: false, nextbtn: false },
+    { row: 'Wie war Ihre Müdigkeit beim Zubettgehen? (1=keine Müdigkeit / 8=starke Müdikgeit)', answer: '0', set: false, nextbtn: false },
+    { row: 'Antwort 3', answer: '3', set: false, nextbtn: false },
+    { row: 'Wann sind Sie zu Bett gegangen?', answer: '0', set: false, nextbtn: false },
+    { row: 'Antwort 4', answer: '4', set: false, nextbtn: false },
+    { row: 'Was haben Sie noch im Bett gemacht?', answer: '0', set: false, nextbtn: false },
+    { row: 'Antwort 5', answer: '5', set: false, nextbtn: false },
+    { row: 'Wann haben Sie das Licht gelöscht?', answer: '0', set: false, nextbtn: false },
+    { row: 'Antwort 6', answer: '6', set: false, nextbtn: false },
+    { row: 'Geschätzte Einschlafdauer', answer: '0', set: false, nextbtn: false },
+    { row: 'Antwort 7', answer: '7', set: false, nextbtn: false },
+    { row: 'Wie oft sind Sie aufgewacht?', answer: '0', set: false, nextbtn: false },
+    { row: 'Antwort 8', answer: '8', set: false, nextbtn: false },
+    { row: 'Wie lange waren Sie dann jeweils wach?', answer: '0', set: false, nextbtn: false },
+    { row: 'Antwort 9', answer: '9', set: false, nextbtn: false },
+    { row: 'Wann sind Sie endgültig aufgewacht?', answer: '0', set: false, nextbtn: false },
+    { row: 'Antwort 10', answer: '10', set: false, nextbtn: false },
+    { row: 'Wann sind Sie morgens aufgestanden?', answer: '0', set: false, nextbtn: false },
+    { row: 'Antwort 11', answer: '11', set: false, nextbtn: false },
+    { row: 'Wie lange haben Sie geschlafen?', answer: '0', set: false, nextbtn: false },
+    { row: 'Antwort 12', answer: '12', set: false, nextbtn: false },
+    { row: 'Wie lange haben Sie insgesamt im Bett gelegen?', answer: '0', set: false, nextbtn: false },
+    { row: 'Antwort 13', answer: '13', set: false, nextbtn: false },
+    { row: 'Haben Sie Schlafmittel genommen?', answer: '0', set: false, nextbtn: false },
+    { row: 'Antwort 14', answer: '14', set: false, nextbtn: false },
+  ];
+
+// This function will allow you to make a ion-input become a simple text display when users finishes to input the phoneNotice item
+next(index){
+    // We set the phoneNotice at given index completed
+    this.questions[index + 1].set = true;
+    this.questions[index + 2].set = true;
+    this.questions[index].nextbtn = false;
+    this.questions[index + 2].nextbtn = true;
+    
+}
 }
